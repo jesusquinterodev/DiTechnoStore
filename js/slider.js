23356e8a02cb5904a77e5864fbd74e27cf35a61e
Jesus Quintero <jaqs2602@gmail.com>
2022-10-24 15:08:29 -0300

    Primeros cambios para hacer el nav responsive

--- js/slider.js
+++ js/slider.js
@@ -11,7 +11,7 @@ function Next() {
   let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
   slider.style.marginLeft = "-200%";
   slider.style.transition = "all 0.5s";
-  setTimeout(function(){
+  setTimeout(function () {
     slider.style.transition = "none";
     slider.insertAdjacentElement("beforeend", sliderSectionFirst);
     slider.style.marginLeft = "-100%";
@@ -23,21 +23,21 @@ function Prev() {
   let sliderSectionLast = sliderSection[sliderSection.length - 1];
   slider.style.marginLeft = "0%";
   slider.style.transition = "all 0.5s";
-  setTimeout(function(){
+  setTimeout(function () {
     slider.style.transition = "none";
     slider.insertAdjacentElement("afterbegin", sliderSectionLast);
     slider.style.marginLeft = "-100%";
   }, 500);
 }
 
-btnRight.addEventListener("click", function(){
+btnRight.addEventListener("click", function () {
   Next();
 });
 
-btnLeft.addEventListener("click", function(){
+btnLeft.addEventListener("click", function () {
   Prev();
 });
 
-setInterval(function(){
+setInterval(function () {
   Next();
 }, 5000);
